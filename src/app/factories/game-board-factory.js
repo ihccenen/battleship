ae364a18a1681116d2580bfa7b5377d63458b390
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-01-21 19:15:02 -0300

    Added allShipsHaveBeenSunk

--- src/app/factories/game-board-factory.js
+++ src/app/factories/game-board-factory.js
@@ -4,6 +4,7 @@ const GameBoard = () => {
   const board = new Map();
   const missesList = new Set();
   const hitsLis = new Set();
+  let remainingShips = 0;
 
   for (let i = 0; i < 10; i += 1) {
     for (let j = 0; j < 10; j += 1) {
@@ -19,7 +20,9 @@ const GameBoard = () => {
         if (!board.has(node) || board.get(node) != null) return false;
       }
 
-      const ship = Ship();
+      const ship = Ship(length);
+      remainingShips += 1;
+
       for (let i = coordinates[1]; i < coordinates[1] + length; i += 1) {
         board.set(`${coordinates[0]}, ${i}`, ship);
       }
@@ -42,9 +45,15 @@ const GameBoard = () => {
         return 'Shot missed.';
       }
 
+      ship.hit();
       hitsLis.add(coord);
 
+      if (ship.isSunk()) remainingShips -= 1;
+
       return 'Shot hits.';
+    },
+    allShipsHaveBeenSunk() {
+      return remainingShips < 1;
     },
   };
 };
