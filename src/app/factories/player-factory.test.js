282743db1bcdae4b5cf88a7fc94820b1b39ea1dd
ihccenen <110120407+ihccenen@users.noreply.github.com>
2023-01-22 18:17:39 -0300

    Added player factory tests

---
+++ src/app/factories/player-factory.test.js
@@ -0,0 +1,37 @@
+import Player from './player-factory';
+
+test('player legal move', () => {
+  const p1 = Player();
+  const enemy = Player();
+
+  enemy.placeShip({ length: 2, coordinates: [1, 1] });
+
+  const result = p1.attackEnemyBoard([1, 1], enemy.receiveEnemyAttack);
+
+  expect(result).toBe(true);
+});
+
+test('AI random legal move', () => {
+  const enemy = Player();
+  const ai = Player();
+
+  const result = ai.randomAttackEnemyBoard(enemy.receiveEnemyAttack);
+
+  expect(result).toBe(true);
+});
+
+test('AI sunk the only ship on the board with random moves', () => {
+  const enemy = Player();
+  const ai = Player();
+  let result = false;
+
+  enemy.placeShip({ length: 2, coordinates: [1, 1] });
+
+  while (!result) {
+    ai.randomAttackEnemyBoard(enemy.receiveEnemyAttack);
+
+    result = enemy.allShipsHaveBeenSunk();
+  }
+
+  expect(result).toBe(true);
+});
